166c1b0e46f889afa3298e94fbf1f3f96a88c4ae
Zachary Bryce <37067686+nylar357@users.noreply.github.com>
2020-07-25 03:37:51 -0400

    Update quine.py

--- quine.py
+++ quine.py
@@ -1,7 +1,7 @@
 
-The Elegant Quine
-Self Replicating Python Code
-Keep in mind to truly be a quine, it cannot be executed outside a python shell, otherwise the input is not outputting a copy
+### The Elegant Quine
+### Self Replicating Python Code
+### Keep in mind to truly be a quine, it cannot be executed outside a python shell, otherwise the input is not outputting a copy
 
 
-	s='s=%r;print(s%%s)';print(s%s)
+						s='s=%r;print(s%%s)';print(s%s)
